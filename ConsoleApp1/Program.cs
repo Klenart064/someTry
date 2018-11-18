a902726cdcb68768bb14a5a39255a460da6796a2
Klenart064 <Klenart064@gmail.com>
2018-11-18 20:29:56 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double Tol = 0.1;
+            int Sgib = 0;
+            string name = "Миллиметров";
+            int num = 41;
+            while (Sgib <= num)
+            {
+                Tol = Tol * 2;
+                Sgib++;
+            }
+
+            if (Tol >= 1000 && Tol < 10000)
+            {
+                Tol = Tol / 1000;
+                name = "Сантиметров";
+            }
+            else if (Tol == 10000 && Tol < 100000)
+            {
+                Tol = Tol / 10000;
+                name = "Метров";
+            }
+            else if (Tol >= 100000)
+            {
+                Tol = Tol / 100000;
+                name = "Километров";
+            }
+
+            Console.WriteLine("Толщина = " + Tol + name);
+            Console.ReadLine();
+        }
+    }
+}
